7d73c0d765189e20d6ca9eb6506a61abc09bdc24
Nicholas Ramsay <nick@yousponsor.me>
2021-10-06 18:38:44 +1100

    added separated error handling outside of executor

---
+++ src/Errors.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "include/Runtime.h"
+#include "include/Errors.h"
+
+int HandleError(Runtime r) {
+    switch (r->error_type) {
+    case ERROR_SYNTAX:
+        printf("Error: Syntax error (line: %d).\n", r->line_num);
+        return 0;
+    case ERROR_COND:
+        printf("Error: Conditional error (line: %d).\nNote: Is there an incorrectly structured conditional sequence?\n", r->line_num);
+        return 0;
+    case ERROR_DIVISION:
+        printf("Error: Division by zero error (line: %d).\n", r->line_num);
+        break;
+    case ERROR_COND_LOOP:
+        printf("Error: Conditional loop error (line: %d).\nNote: Loops cannot be declared conditionally.", r->line_num);
+        return 0;
+    default:
+        printf("Error: Unknown error (line: %d).\n", r->line_num);
+        return 0;
+    }
+}
+
---
+++ src/include/Errors.h
@@ -0,0 +1,8 @@
+#ifndef ERRORS_H
+#define ERRORS_H
+
+#include "Runtime.h"
+
+int HandleError(Runtime r);
+
+#endif
